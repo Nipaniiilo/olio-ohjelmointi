16777bd209134e76f0cdb675fe8b3559eacf5d0e
Nico <tuomelanico@gmail.com>
2024-11-14 20:40:34 +0200

    h2b valmis

--- h2b/car.cpp
+++ h2b/car.cpp
@@ -1,3 +1,19 @@
 #include "car.h"
+#include <iostream>
+using namespace std;
 
-Car::Car() {}
+
+
+
+Car::Car(string a, string b, int i)
+{
+    brand=a;
+    model=b;
+    yearModel=i;
+}
+
+void Car::printData() const
+{
+    cout<<"brand: "<<brand<< ", model: "<<model<<", yearmodel: "<<yearModel<<endl;
+}
+
--- h2b/main.cpp
+++ h2b/main.cpp
@@ -1,9 +1,29 @@
+#include "car.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<Car> carList;
+
+    Car objectCar0("Bmw","550", 2010);
+    Car objectCar1("Volvo","740", 1995);
+    Car objectCar2("Audi","A6", 2002);
+
+    carList.push_back(objectCar0);
+    carList.push_back(objectCar1);
+    carList.push_back(objectCar2);
+
+    cout<<"Listan toisen alkion tiedot: "<<endl;
+    carList[1].printData();
+
+     cout<<"Koko autolistan tiedot: "<<endl;
+
+    for(int x=0; x<=2; x++){
+        carList[x].printData();
+        }
     return 0;
+
 }
